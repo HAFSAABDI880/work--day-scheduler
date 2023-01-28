2c9aa9b6650bbb032aae1a47d27fb9a51c44a962
HAFSAABDI880 <119974289+HAFSAABDI880@users.noreply.github.com>
2023-01-28 17:17:29 +0000

    rendering current date

--- script.js
+++ script.js
@@ -5,9 +5,29 @@ let textBox = $("textarea");
 $(document).ready(function(){
     $(".saveBtn").on("click", function(){
         
-        let = userText = $(this).siblings(".description").val();
+    let = userText = $(this).siblings(".description").val();
         let = time = $(this).parent().attr("id");
         
         localStorage.setItem(time, userText);
 
  });
+ $("#9am .description").val(localStorage.getItem("9am"));
+ $("#10am .description").val(localStorage.getItem("10am"));
+ $("#11am .description").val(localStorage.getItem("11am"));
+ $("#12pm .description").val(localStorage.getItem("12pm"));
+ $("#13pm .description").val(localStorage.getItem("13pm"));
+ $("#14pm .description").val(localStorage.getItem("14pm"));
+ $("#15pm .description").val(localStorage.getItem("15pm"));
+ $("#16pm .description").val(localStorage.getItem("16pm"));
+ $("#17pm .description").val(localStorage.getItem("17pm"));
+ $("#18pm .description").val(localStorage.getItem("18pm"));
+ $("#19pm .description").val(localStorage.getItem("19pm"));
+ $("#20pm .description").val(localStorage.getItem("20pm"));
+ $("#21pm .description").val(localStorage.getItem("21pm"));
+});
+
+let date = moment().format("MMMM Do, YYYY");
+let currentDate = function(){
+    $("#currentDay").text(date);
+};
+currentDate();
